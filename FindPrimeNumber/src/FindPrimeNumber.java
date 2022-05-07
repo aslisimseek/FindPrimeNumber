2a2ce511d8b60a1af0e12f05f8f6b2789a9db95e
Aslıhan Şimşek <simseekasli@gmail.com>
2022-05-07 17:00:38 +0300

    Change user name for github

--- FindPrimeNumber/src/FindPrimeNumber.java
+++ FindPrimeNumber/src/FindPrimeNumber.java
@@ -1,5 +1,7 @@
 public class FindPrimeNumber {
     public static void main(String[] args) {
+
+        
         for (int i = 1; i <= 100; i++) {
             boolean isPrimeNumber = true;
             for (int j = 2; j < i; j++) {
